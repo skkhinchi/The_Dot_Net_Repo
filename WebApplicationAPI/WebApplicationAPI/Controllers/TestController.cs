7021c4aaa2e572c8e9e782cb8a23824875d8079a
Sumit Kumar <Sumit.Kumar3@honeywell.com>
2022-07-06 16:28:12 +0530

    Custom Filter added

--- WebApplicationAPI/WebApplicationAPI/Controllers/TestController.cs
+++ WebApplicationAPI/WebApplicationAPI/Controllers/TestController.cs
@@ -1,9 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+
 
 namespace WebApplicationAPI.Controllers
 {
@@ -15,67 +11,47 @@ namespace WebApplicationAPI.Controllers
         abstract int Add(int a, int b);
 
     }
-    class Math : IMath
+   
+    [ApiController]
+    [Route("[controller]")]
+    public class Math : ControllerBase, IMath
     {
-
+        [Route("Welcome")]
+        [HttpGet]
+        public string Display()
+        {
+            return "Welcome to DotNet";
+        }
+        [Route("Multiply")]
+        [HttpPost]
         public int Multiply(int a, int b)
         {
             return a * b;
         }
 
+
+        [SampleActionFilter]
+        [Route("Divide")]
+        [HttpPost]
         public int Divide(int a, int b)
         {
             return a / b;
         }
 
+        [Route("Substract")]
+        [HttpPost]
         public int Substract(int a, int b)
         {
             return a - b;
         }
 
+        [Route("Add")]
+        [HttpPost]
         public int Add(int a, int b)
         {
             return a + b;
         }
-    }
 
-    [ApiController]
-    [Route("[controller]")]
-    public class TestController : ControllerBase
-    {
-
-        Math obj = new Math();
-
-        [HttpGet]
-        public dynamic Get(int a, int b, string c)
-        {
-            switch(c)
-            {
-                case "add":
-                    {
-                        return obj.Add(a, b);
-                    }
-                case "sub":
-                    {
-                        return obj.Substract(a, b);
-                    }
-                case "multi":
-                    {
-                        return obj.Multiply(a, b);
-                    }
-                case "div":
-                    {
-                        return obj.Divide(a, b);
-                    }
-                default:
-                    {
-                        return "Something went wrong";
-                    }
-
-            }
-          
-;
-        }
 
 
 
---
+++ WebApplicationAPI/WebApplicationAPI/SampleActionFilter.cs
@@ -0,0 +1,28 @@
+﻿
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Diagnostics;
+
+namespace WebApplicationAPI
+{
+    public class SampleActionFilter : ActionFilterAttribute, IActionFilter
+    {
+        public void onException(ExceptionContext exceptionContext)
+        {
+              throw new DivideByZeroException();
+        }
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            // Do something before the action executes.
+            throw new DivideByZeroException();
+
+        }
+
+        public void OnActionExecuting(ActionExecutedContext filtercontext)
+        {
+            throw new DivideByZeroException();
+            // Do something after the action executes.
+        }
+    }
+
+}
